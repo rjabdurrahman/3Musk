f75880bcecde93ed0e58cb703919d3213ef16c69
rjabdurrahman <md.rahman0296@gmail.com>
2018-02-16 20:35:18 +0600

    Probem 3 (A) Solved

--- Program.cpp
+++ Program.cpp
@@ -3,11 +3,16 @@
 using namespace std;
 int main()
 {
-    int s;
-    cin >> s;
-    cout << s/3600 << ":";
-    cout << (s%3600)/60 << ":";
-    cout << (s%3600)%60 << endl;
+    int m;
+    cin >> m;
+    cout << m << endl;
+    cout << m/100 << " nota(s) de R$ 100,00" << endl;
+    cout << (m%100)/50 << " nota(s) de R$ 50,00" << endl;
+    cout << ((m%100)%50)/20 << " nota(s) de R$ 20,00" << endl;
+    cout << (((m%100)%50)%20)/10 << " nota(s) de R$ 10,00" << endl;
+    cout << ((((m%100)%50)%20)%10)/5 << " nota(s) de R$ 5,00" << endl;
+    cout << (((((m%100)%50)%20)%10)%5)/2 << " nota(s) de R$ 2,00" << endl;
+    cout << (((((m%100)%50)%20)%10)%5)%2 << " nota(s) de R$ 1,00" << endl;
 
     return 0;
 }
